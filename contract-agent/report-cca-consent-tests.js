d2aa8080685d36cc74503720ad6cd3e3df955c28
mathieu <mathieu@visionspol.eu>
2025-01-23 10:46:41 +0100

    chore: review reports generator

--- contract-agent/report-cca-consent-tests.js
+++ contract-agent/report-cca-consent-tests.js
@@ -5,40 +5,43 @@ const testDir = './src/tests';
 const testFiles = fs.readdirSync(testDir).filter(file => file.match(/consent\..+\.spec\.ts$/));
 
 (async () => {
- if (!fs.existsSync('mochawesome-report/consent')) {
-   fs.mkdirSync('mochawesome-report/consent', { recursive: true });
- }
+  const consentDir = 'mochawesome-report/consent';
+  if (fs.existsSync(consentDir)) {
+    fs.rmSync(consentDir, { recursive: true, force: true });
+  }
+  
+  fs.mkdirSync(consentDir, { recursive: true });
 
- for (const file of testFiles) {
-   console.log(`Running test: ${file}`);
-   await new Promise((resolve, reject) => {
-     exec(`npx ts-mocha -p tsconfig.json ${path.join(testDir, file)} --timeout 4000 --reporter mochawesome --reporter-options reportDir=mochawesome-report/consent,reportFilename=${file} --exit`, (err, stdout, stderr) => {
-       if (err) {
-         console.error(`Error in ${file}:\n`, stderr);
-         return reject(err);
-       }
-       console.log(stdout);
-       resolve();
-     });
-   });
- }
+  for (const file of testFiles) {
+    console.log(`Running test: ${file}`);
+    await new Promise((resolve, reject) => {
+      exec(`npx ts-mocha -p tsconfig.json ${path.join(testDir, file)} --timeout 4000 --reporter mochawesome --reporter-options reportDir=mochawesome-report/consent,reportFilename=${file} --exit`, (err, stdout, stderr) => {
+        if (err) {
+          console.error(`Error in ${file}:\n`, stderr);
+          return reject(err);
+        }
+        console.log(stdout);
+        resolve();
+      });
+    });
+  }
 
- await new Promise((resolve, reject) => {
-   exec(`npx mochawesome-merge "mochawesome-report/consent/*.json" -o mochawesome-report/consent/mochawesome.json`, async (err) => {
-     if (err) {
-       console.error(err);
-       return reject(err);
-     }
-     
-     exec(`npx mochawesome-report-generator mochawesome-report/consent/mochawesome.json -o mochawesome-report/consent -f report`, (err) => {
-       if (err) {
-         console.error(err);
-         return reject(err);
-       }
-       resolve();
-     });
-   });
- });
+  await new Promise((resolve, reject) => {
+    exec(`npx mochawesome-merge "mochawesome-report/consent/*.json" -o mochawesome-report/consent/mochawesome.json`, async (err) => {
+      if (err) {
+        console.error(err);
+        return reject(err);
+      }
+      
+      exec(`npx mochawesome-report-generator mochawesome-report/consent/mochawesome.json -o mochawesome-report/consent -f report`, (err) => {
+        if (err) {
+          console.error(err);
+          return reject(err);
+        }
+        resolve();
+      });
+    });
+  });
 
- console.log('All tests completed and report generated.');
+  console.log('All tests completed and report generated.');
 })();
--- contract-agent/report-cca-contract-tests.js
+++ contract-agent/report-cca-contract-tests.js
@@ -5,40 +5,43 @@ const testDir = './src/tests';
 const testFiles = fs.readdirSync(testDir).filter(file => file.match(/contract\..+\.spec\.ts$/));
 
 (async () => {
- if (!fs.existsSync('mochawesome-report/contract')) {
-   fs.mkdirSync('mochawesome-report/contract', { recursive: true });
- }
+  const contractDir = 'mochawesome-report/contract';
+  if (fs.existsSync(contractDir)) {
+    fs.rmSync(contractDir, { recursive: true, force: true });
+  }
+  
+  fs.mkdirSync(contractDir, { recursive: true });
 
- for (const file of testFiles) {
-   console.log(`Running test: ${file}`);
-   await new Promise((resolve, reject) => {
-     exec(`npx ts-mocha -p tsconfig.json ${path.join(testDir, file)} --timeout 4000 --reporter mochawesome --reporter-options reportDir=mochawesome-report/contract,reportFilename=${file} --exit`, (err, stdout, stderr) => {
-       if (err) {
-         console.error(`Error in ${file}:\n`, stderr);
-         return reject(err);
-       }
-       console.log(stdout);
-       resolve();
-     });
-   });
- }
+  for (const file of testFiles) {
+    console.log(`Running test: ${file}`);
+    await new Promise((resolve, reject) => {
+      exec(`npx ts-mocha -p tsconfig.json ${path.join(testDir, file)} --timeout 4000 --reporter mochawesome --reporter-options reportDir=mochawesome-report/contract,reportFilename=${file} --exit`, (err, stdout, stderr) => {
+        if (err) {
+          console.error(`Error in ${file}:\n`, stderr);
+          return reject(err);
+        }
+        console.log(stdout);
+        resolve();
+      });
+    });
+  }
 
- await new Promise((resolve, reject) => {
-   exec(`npx mochawesome-merge "mochawesome-report/contract/*.json" -o mochawesome-report/contract/mochawesome.json`, async (err) => {
-     if (err) {
-       console.error(err);
-       return reject(err);
-     }
-     
-     exec(`npx mochawesome-report-generator mochawesome-report/contract/mochawesome.json -o mochawesome-report/contract -f report`, (err) => {
-       if (err) {
-         console.error(err);
-         return reject(err);
-       }
-       resolve();
-     });
-   });
- });
+  await new Promise((resolve, reject) => {
+    exec(`npx mochawesome-merge "mochawesome-report/contract/*.json" -o mochawesome-report/contract/mochawesome.json`, async (err) => {
+      if (err) {
+        console.error(err);
+        return reject(err);
+      }
+      
+      exec(`npx mochawesome-report-generator mochawesome-report/contract/mochawesome.json -o mochawesome-report/contract -f report`, (err) => {
+        if (err) {
+          console.error(err);
+          return reject(err);
+        }
+        resolve();
+      });
+    });
+  });
 
- console.log('All tests completed and report generated.');
+  console.log('All tests completed and report generated.');
 })();
